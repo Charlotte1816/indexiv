d55ea5473f9a906cd391e42ded110373dc422904
Charlotte1816 <integ8983dx@outlook.jp>
2026-02-16 02:06:47 +0900

    260216

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Astro.site値
+    site: "https://indexiv.org",
+    // i18n設定
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'ja'],
+        routing: {
+            prefixDefaultLocale: true,
+        }
+    },
+});
